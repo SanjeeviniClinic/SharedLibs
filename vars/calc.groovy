62b93efec75b32a3aa448625923909dde4b563a5
DayanandJB <90151660+DayanandJB@users.noreply.github.com>
2021-10-17 22:18:09 +0530

    Update calc.groovy

--- vars/calc.groovy
+++ vars/calc.groovy
@@ -1,9 +1,10 @@
 def add(num1,num2)
 {
-  echo "Sum of $num1 + $num2 = $num1+$num2
+  echo "Sum of $num1 and $num2 = $num1+$num2"
+  echo ""
 }
 
 def mul(num1,num2)
 {
-  echo "Sum of $num1 * $num2 = $num1*$num2
+  echo "Mul of $num1 and $num2 = $num1*$num2"
 }
